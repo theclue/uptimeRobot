01f3dcfb6b0b5ce9fcafe25aa9a749c7e20e6021
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2015-09-30 00:50:34 +0200

    start cleaning-up roxygen

--- R/uptimerobot.account.details.R
+++ R/uptimerobot.account.details.R
@@ -1,22 +1,21 @@
-#' @rdname uptimerobot.account.details
-#' @export
-#'
-#' @title 
 #' Get the account details for who is linked to the given API key
 #'
-#' @description
 #' \code{uptimerobot.account.details} returns a list or a vector with the account details connected to the given api key.
 #' 
-#' @author
-#' Gabriele Baldassarre
+#' @author Gabriele Baldassarre
+#' 
+#' @return A list or a vector with the account details.
 #'
 #' @param api.key a string with a valid key for connecting to UptimeRobors public API.
 #' @param unlist logical. Set to \code{TRUE} to unlist the output to a named vector, \code{FALSE} to get a named list.
-#' 
+#'
+#' @importFrom RCurl getURL
+#' @importFrom rjson fromJSON
+#' @export 
 uptimerobot.account.details <- function(api.key, unlist = FALSE){
   
-  data <- rjson::fromJSON(
-    RCurl::getURL(
+  data <- fromJSON(
+    getURL(
       paste0("https://api.uptimerobot.com/getAccountDetails?apiKey=",
              api.key,
              "&format=json&noJsonCallback=1"
--- R/uptimerobot.contact.delete.R
+++ R/uptimerobot.contact.delete.R
@@ -1,25 +1,23 @@
-#' @rdname uptimerobot.contact.delete
-#' @export
-#'
-#' @title 
 #' Delete an alert contact
 #'
-#' @description
-#' \code{uptimerobot.contact.delete} remove an alert contanct, unlinking from all the registered monitors.
+#' \code{uptimerobot.contact.delete} removes an alert contanct, unlinking from all the registered monitors.
 #' 
-#' The function returns \code{TRUE} in case success. An error is thrown otherwise.
+#' @return The function returns \code{TRUE} in case success. An error is thrown otherwise.
 #'  
 #' @author
 #' Gabriele Baldassarre
 #' 
 #' @param api.key string with a valid key for connecting to Uptimerobot API.
 #' @param id numeric or integer with the ID of the contact to delete.
-#' 
+#'
+#' @importFrom RCurl getURL
+#' @importFrom rjson fromJSON
+#' @export 
 uptimerobot.contact.delete <- function(api.key, id){
   
   
-  data <- rjson::fromJSON(
-    RCurl::getURL(
+  data <- fromJSON(
+    getURL(
       paste0("https://api.uptimerobot.com/deleteAlertContact?apiKey=",
              api.key,
              "&alertContactID=", id,
