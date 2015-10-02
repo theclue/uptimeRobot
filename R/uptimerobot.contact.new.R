2637cad39d54857f47437fdf6a3444ecd0f2e93b
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2015-10-02 17:27:55 +0200

    code and doc cleanup

--- R/uptimerobot.contact.new.R
+++ R/uptimerobot.contact.new.R
@@ -5,14 +5,29 @@
 #' @details
 #' The alert contacts are whom to be notified when the monitor goes up/down. 
 #' 
+#' The index lookup keys and values are available on the Uptimerobot API page on \url{https://uptimerobot.com/api}
+#' 
 #' @return The function returns the ID of the newly created contact in case success. An error is thrown otherwise.
+#' 
+#' @seealso \code{\link{uptimerobot.contacts}}, \code{\link{uptimerobot.contact.delete}}
 #' 
 #' @author Gabriele Baldassarre
 #' 
 #' @param api.key string with a valid key for connecting to Uptimerobot API.
-#' @param type string or integer with the type of the contact. You can use both the friendly name (string) or the index (integer) here.
+#' @param type string or numeric with the type of the contact. You can use both the value (string) or the index (numeric) here.
 #' @param value string with the value of the contact (ie. the email address).
 #' @param friendly.name string the friendly (screen) name of the contact.
+#'
+#' @examples
+#' \dontrun{
+#'  # Let's assume the api.key is available into the environment variable KEY
+#'  api.key <- Sys.getenv("KEY", "")
+#'  # Create a new contact and get the ID
+#'  contact.new <- uptimerobot.contact.new(api.key, type = "email", value = "foo@@bar.com", "John Doe")
+#' 
+#'  # Get informations about this new contact
+#'  contact.detail <- uptimerobot.contacts(api.key, contacts = contact.new)
+#' }
 #' 
 #' @importFrom RCurl getURL
 #' @importFrom rjson fromJSON
@@ -21,6 +36,11 @@ uptimerobot.contact.new <- function(api.key,
                                     type,
                                     value,
                                     friendly.name){
+  
+  if(is.null(api.key) | 
+     is.na(api.key) | 
+     (is.character(api.key) & nchar(api.key)==0)
+  ) stop("api.key cannot be empty or NULL")
   
   # Decode contact type
   if(!(is.null(type))) {
